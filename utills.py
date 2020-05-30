45e0b3148c7a4c27b45e237fde4c7a9cd590cde8
Deepak Birla <birla.deepak26@gmail.com>
2020-05-30 20:15:06 +0530

    Update utills.py

--- utills.py
+++ utills.py
@@ -20,13 +20,13 @@ import cv2
 import numpy as np
 
 # Function to calculate bottom center for all bounding boxes and transform prespective for all points.
-def get_transformed_points(boxes, mi):
+def get_transformed_points(boxes, prespective_transform):
     
     bottom_points = []
     for box in boxes:
         pnts = np.array([[[int(box[0]+(box[2]*0.5)),int(box[1]+box[3])]]] , dtype="float32")
         #pnts = np.array([[[int(box[0]+(box[2]*0.5)),int(box[1]+(box[3]*0.5))]]] , dtype="float32")
-        bd_pnt = cv2.perspectiveTransform(pnts, mi)[0][0]
+        bd_pnt = cv2.perspectiveTransform(pnts, prespective_transform)[0][0]
         pnt = [int(bd_pnt[0]), int(bd_pnt[1])]
         bottom_points.append(pnt)
         
